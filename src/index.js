45620d3ac3a4f38fbd9f1c81df0aa1b9b894b02c
Lesja01 <Lesja01@mail.ru>
2019-03-13 19:31:42 +0300

    fix: add solutions for function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ // your implementation
+ let n = number;
+ let num = 2;
+ let arr = [];
+ 
+ function getEasyBase (b) {
+	 if (b === 1) {
+			 return
+			 }
+			if (b < num) {
+			 return
+			 }else if(b % num === 0){
+		arr=[...arr, num];
+		b = b / num;
+		getEasyBase(b);
+		}else if(b > num){		
+		 num++;
+		 getEasyBase(b);
+	 }	
+ return arr
+}
+let easyBase = getEasyBase(base);
+
+function getSum(easyBase){
+let all=[];
+let repeat=1;
+for (let i=0; i<easyBase.length; i++){
+	let sum = 0;
+	if(easyBase[i]==easyBase[i+1]){
+		repeat++
+	}else{ 
+		repeat=1;
+	}
+	for (let j=1; n>=Math.pow(easyBase[i],j);j++){    
+		sum = sum + Math.floor(n/Math.pow(easyBase[i],j));       
+		}       
+		var result=Math.floor(sum/repeat);			
+		all=[...all, result]		 
+	}return all
+}
+let allSum=getSum(easyBase);
+		
+let minZero=Math.min.apply(null, allSum)
+return minZero
 }
